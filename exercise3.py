54aa23703fa09a8686d256497b1cd7304e1639d8
Anton <anton.mois33v@gmail.com>
2019-02-11 14:41:59 -0500

    Added more lines of code to exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,10 @@
+# ask for the user's name
 print("What is your name?")
+user_name = input()
+print("Hello, {}".format(user_name))
+
+#
+print("How old are you?")
+age = input()
+year_born = 2019 - int(age)
+print("Given that you are {} years old, you must have been born in {}.".format(age, year_born))
