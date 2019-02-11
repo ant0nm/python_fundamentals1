2f751da716482bc4ec1f63157ea93960b0bbda74
Anton <anton.mois33v@gmail.com>
2019-02-11 15:52:42 -0500

    Completed part 1 of exercise 4

---
+++ exercise4_1.py
@@ -0,0 +1,9 @@
+# user enter a number
+print("Hi there, please enter a number:")
+number_str = input()
+# number entered vs. 100
+number = int(number_str)
+if number >= 100:
+    print("that's a big number!")
+else:
+    print("why not dream a little bigger?")
