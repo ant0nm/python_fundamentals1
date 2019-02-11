bc82b8a8fe2f48755be9ffbef9fc92231a6d4fd9
Anton <anton.mois33v@gmail.com>
2019-02-11 13:36:23 -0500

    Completed exercise 2

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+# problem 1: tipping
+print("An {}% tip for a $55 meal is about ${}".format(18, 55*0.18))
+
+# problem 2: data type conversion
+print("I was born in " + str(1994))
+
+# problem 3: string interpolation
+print("45628 x 7839 = {}".format(45628*7839))
+
+# problem 4: logical statement
+print((10 < 20 and 30 < 20) or not(10 == 11))
